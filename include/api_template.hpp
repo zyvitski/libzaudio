6355ea8fffe57130a11a4c67257932c0b4f5b54e
Alex Zywicki <alexander.zywicki@gmail.com>
2016-12-29 03:13:31 -0600

    moved api_template into include dir

--- include/api_template.hpp
+++ include/api_template.hpp
@@ -1,5 +1,21 @@
 #ifndef API_TEMPLATE
 #define API_TEMPLATE
+/*
+This file is part of zaudio.
+
+    zaudio is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    zaudio is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with zaudio.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 
 #include <zaudio.hpp>
