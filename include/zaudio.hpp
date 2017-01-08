53008fcadea540e376d88b8cd1360313f4355b81
Alex Zywicki <alexander.zywicki@gmail.com>
2017-01-07 23:23:51 -0500

    fixes to build

--- include/zaudio.hpp
+++ include/zaudio.hpp
@@ -19,8 +19,6 @@ This file is part of zaudio.
     along with zaudio.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
-
 #include "sample_utility.hpp"
 #include "time_utility.hpp"
 #include "error_utility.hpp"
@@ -31,26 +29,7 @@ This file is part of zaudio.
 #include "audio_stream.hpp"
 #include "audio_process.hpp"
 #include "pa_stream_api.hpp"
-
 #include "zaudio_defaults.hpp"
-
-
-/*!
-*\note At least one stream api must be present on every platform.
-       The provided api is permitted to be a non functional dummy api for platforms where a proper api is impossible
-       Preferabbly on any platform that has a filesystem, a file codec api will be provided
-       Preferabbly on any platform that has an audio system an hardware api will be provided
-
-       IDEA:
-            Network serialization api?
-                an api that is designed to work with the std::networking proposal for sending audio over network
-
-       All of the apis will share a uniform interface
-       Ideally multiple apis can be used at once
-            TODO: come up with endpoint class to make multi api io possible
-*/
-
-
 
 
 #endif
