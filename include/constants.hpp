ad4f2be1cd7eedd28b9f04fc7bf3f04437630689
Alex Zywicki <alexander.zywicki@gmail.com>
2017-01-28 20:14:10 -0500

    fixed pi

--- include/constants.hpp
+++ include/constants.hpp
@@ -19,10 +19,10 @@ This file is part of zaudio.
 namespace zaudio
 {
     constexpr static double pi = 3.14159265358979323846264338327950288419716939937510;
-    constexpr static double π  = pi;
+    //constexpr static double π  = pi;
 
     constexpr static double two_pi = 2.0 * pi;
-    constexpr static double two_π  = two_pi;
+    //constexpr static double two_π  = two_pi;
 }
 
 #endif
